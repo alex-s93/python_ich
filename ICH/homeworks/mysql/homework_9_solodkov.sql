009db8da867f2f4ca63ff38a82bb721943b244f3
alex-s93 <solodkov.alex@gmail.com>
2024-03-26 16:22:25 +0100

    fix for MySQL HW 9

--- ICH/homeworks/mysql/homework_9_solodkov.sql
+++ ICH/homeworks/mysql/homework_9_solodkov.sql
@@ -31,9 +31,10 @@ where year(ODATE) = 2022;
 
 -- Определить, сколько покупок было совершено в каждый день. Отсортировать строки в порядке возрастания количества
 -- покупок. Вывести два поля - дату и количество покупок
-select odate, count(ORDER_ID) as order_count from ORDERS
-group by odate;
+select ODATE, count(ORDER_ID) as order_count from ORDERS
+group by ODATE
+order by order_count;
 
 --  Определить среднюю стоимость покупок в апреле
 select avg(AMT) as avg_amt from ORDERS
-where MONTH(odate) = 4;
+where MONTH(ODATE) = 4;
