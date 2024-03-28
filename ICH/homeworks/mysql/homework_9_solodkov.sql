79e5431e9da615eb504a5cf1175f8d4cd3a1e2a5
alex-s93 <solodkov.alex@gmail.com>
2024-03-28 09:24:21 +0100

    mysql hw 9: add alternative solutions for one task

--- ICH/homeworks/mysql/homework_9_solodkov.sql
+++ ICH/homeworks/mysql/homework_9_solodkov.sql
@@ -1,40 +1,48 @@
 --  Вывести текущую дату и время.
-select now();
+SELECT NOW();
 
 --  Вывести текущий год и месяц
-select YEAR(now()), MONTH(now());
-select extract(YEAR_MONTH from now());
+SELECT YEAR(NOW()), MONTH(NOW());
+SELECT EXTRACT(YEAR_MONTH FROM NOW());
+# месяц в виде названия
+SELECT DATE_FORMAT(NOW(), '%Y-%M');
+# месяц в виде номера
+SELECT DATE_FORMAT(NOW(), '%Y-%m');
 
 --  Вывести текущее время
-select TIME(now());
+SELECT TIME(NOW());
 
 --  Вывести название текущего дня недели
-select dayname(now());
+SELECT DAYNAME(NOW());
 
 --  Вывести номер текущего месяца
-select month(now());
+SELECT MONTH(NOW());
 
 --  Вывести номер дня в дате “2020-03-18”
-select day(now());
+SELECT DAY(NOW());
 
 --  Подключиться к базе данных shop (которая находится на удаленном сервере).
-use shop;
+USE shop;
 
 --  Определить какие из покупок были совершены апреле (4-й месяц)
-select ORDER_ID, ODATE from ORDERS
-where monthname(ODATE) = 'April';
+SELECT ORDER_ID, ODATE
+FROM ORDERS
+WHERE MONTHNAME(ODATE) = 'April';
 -- where month(ODATE) = 4;
 
 --  Определить количество покупок в 2022-м году
-select count(ORDER_ID) from ORDERS
-where year(ODATE) = 2022;
+SELECT COUNT(ORDER_ID)
+FROM ORDERS
+WHERE YEAR(ODATE) = 2022;
 
 -- Определить, сколько покупок было совершено в каждый день. Отсортировать строки в порядке возрастания количества
 -- покупок. Вывести два поля - дату и количество покупок
-select ODATE, count(ORDER_ID) as order_count from ORDERS
-group by ODATE
-order by order_count;
+SELECT ODATE, COUNT(ORDER_ID) AS order_count
+FROM ORDERS
+GROUP BY ODATE
+ORDER BY order_count;
 
 --  Определить среднюю стоимость покупок в апреле
-select avg(AMT) as avg_amt from ORDERS
-where MONTH(ODATE) = 4;
+SELECT AVG(AMT) AS avg_amt
+FROM ORDERS
+WHERE MONTH(ODATE) = 4;
